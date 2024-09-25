c6c90143236bd930632bc940ed81dcd092c88572
ghost000 <radixblade100@gmail.com>
2024-09-25 23:06:14 +0200

    Add Cmake test CI and impl files.

---
+++ src/water-and-jug.cpp
@@ -0,0 +1,12 @@
+﻿// water-and-jug.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "../include/water-and-jug.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
